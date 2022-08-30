cfeba84c742f06f9d1e9154f058b746bf2dc7230
LucaasOliveira <olucasoliveira013@gmail.com>
2022-08-30 12:20:25 -0300

    add html and script js exercise 01

---
+++ exercise01.js
@@ -0,0 +1,18 @@
+/* 1. Crie uma página HTML simples contendo uma div identificada com
+um id e um botão, depois crie um arquivo javascript com uma
+função addConteudo() que deve ser vinculado ao onclick do botão.
+Ao clicar no botão adicionar na div o texto “Olá Mundo”. */
+
+let btn = document.querySelector("button");
+btn.addEventListener("click", (event) => {
+
+    addConteudo();
+
+});
+
+function addConteudo() {
+    const div = document.querySelector("#content");
+    div.innerHTML = "Olá Mundo";
+
+    btn.innerText = "fui clicado";
+};
